553ef5b9161d29f5634e9c1f246120b248f3dd9d
Stas <seo.zoidberg@gmail.com>
2015-09-18 13:57:42 +0300

    done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There is a set of functions to compute and cache the inverse of a matrix.
+##
+## How to use:
+## 
+## max <- matrix()
+## matrix_object <- makeCacheMatrix(max)
+## cacheSolve(matrix_object)
+## 
+## 
+## This function (makeCacheMatrix) crates the list of 4 function for an matix "object":
+##    get() - returns the x matrix stored in the main function.
+##    set() - can be used to change x matrix
+##    setcache() - saves the inversed matrix of x
+##    getcache() - returns the inversed matrix of x
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ 
+  cache <- NULL
+  
+  set <- function(y){
+    x <<- y
+    cache <<- NULL  }
+  get <- function() x
+  setcache <- function(z) cache <<- z
+  getcache <- function() cache
+  
+  list(set = set, get = get, setcache = setcache, getcache = getcache)
+  
 }
 
-
-## Write a short comment describing this function
+## This function (cacheSolve) computes the inverse of the matrix "object" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  cache <- x$getcache() ## getting inverse matrix
+    if(!is.null(cache)) ## if exists - return it
+      {
+      message("getting cached data")
+      return(cache)
+      }
+    my_matrix <- x$get()            ## else - get the matrix
+    cache <- solve(my_matrix, ...)   ## compute the inverse
+    x$setcache(cache)                 ## save the result
+    cache                              ## and return it
 }
+
+############
+## Here is a test to verify.
+##
+## max <- matrix(c(1:4,8:12), nrow = 3)
+## matrix_object <- makeCacheMatrix(max)
+## cacheSolve(matrix_object)
